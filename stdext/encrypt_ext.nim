3cf06652253ec5a199015ba6c4ecfcd357863166
Amru Rosyada <amru.rosyada@gmail.com>
2020-07-09 22:03:17 +0700

    add encrypt_ext xorEncodeDecode

---
+++ stdext/encrypt_ext.nim
@@ -0,0 +1,17 @@
+#[
+  ZendFlow web framework for nim language
+  This framework if free to use and to modify
+  License: BSD
+  Author: Amru Rosyada
+  Email: amru.rosyada@gmail.com
+  Git: https://github.com/zendbit 
+]#
+
+proc xorEncodeDecode*(data: string, key: string): string =
+  # xor encode decode the data with given key
+  var decodedData = ""
+  for i in 0..<data.len:
+    decodedData &= chr(data[i].uint8 xor key[i mod 4].uint8)
+
+  return decodedData
+
