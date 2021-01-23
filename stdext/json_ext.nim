12fde3f9871dd9bd493ba93e26c265ad705a22cf
Amru Rosyada <amru.rosyada@gmail.com>
2021-01-23 10:25:15 +0700

    add FieldsPair extension

--- stdext/json_ext.nim
+++ stdext/json_ext.nim
@@ -5,6 +5,7 @@ export json
 type
   FieldDesc* = tuple[name: string, nodeKind: JsonNodeKind]
   FieldItem* = tuple[val: string, nodeKind: JsonNodeKind]
+  FieldsPair* = tuple[name: string, val: string, nodeKind: JsonNodeKind]
 
 proc `%`*(fieldDesc: FieldDesc): JsonNode =
  
@@ -12,19 +13,33 @@ proc `%`*(fieldDesc: FieldDesc): JsonNode =
 
 proc fieldsDesc*(j: JsonNode): seq[FieldDesc] =
 
-  result = @[]
   for k, v in j:
     result.add((k, v.kind))
 
 proc fieldsItem*(j: JsonNode): seq[FieldItem] =
 
-  result = @[]
   for k, v in j:
     result.add((k, v.kind))
+
+proc fieldsPair*(j: JsonNode): seq[FieldsPair] =
+  
+  for k, v in j:
+    if v.kind != JString:
+      result.add((k, $v, v.kind))
+    else:
+      result.add((k, v.getStr, v.kind))
 
 proc names*(fieldsDesc: seq[FieldDesc]): seq[string] =
   for f in fieldsDesc:
     result.add(f.name)
+
+proc names*(fieldsPair: seq[FieldsPair]): seq[string] =
+  for f in fieldsPair:
+    result.add(f.name)
+
+proc values*(fieldsPair: seq[FieldsPair]): seq[string] =
+  for f in fieldsPair:
+    result.add(f.val)
 
 proc values*(fieldsItem: seq[FieldItem]): seq[string] =
   for f in fieldsItem:
@@ -63,6 +78,40 @@ proc fieldsDesc*[T](obj: T): seq[FieldDesc] =
         result.add((k, JObject))
       else:
         result.add((k, JNull))
+
+proc fieldsPair*[T](obj: T): seq[FieldsPair] =
+  for k, v in obj.fieldPairs:
+    let vtype = cast[type v](v)
+    if vtype is Option:
+      if vtype is Option[SomeInteger]:
+        result.add((k, $ v.getOrDefault, JInt))
+      elif vtype is Option[SomeFloat]:
+        result.add((k, $ v.getOrDefault, JFloat))
+      elif vtype is Option[string]:
+        result.add((k, $ v.getOrDefault, JString))
+      elif vtype is Option[bool]:
+        result.add((k, $ v.getOrDefault, JBool))
+      elif vtype is Option[array] or vType is Option[seq]:
+        result.add((k, $ v.getOrDefault, JArray))
+      elif vtype is Option[RootObj]:
+        result.add((k, $ v.getOrDefault, JObject))
+      else:
+        result.add((k, $ v.getOrDefault, JNull))
+    else:
+      if v is SomeInteger:
+        result.add((k, $ v.getOrDefault, JInt))
+      elif v is SomeFloat:
+        result.add((k, $ v.getOrDefault, JFloat))
+      elif v is string:
+        result.add((k, $v, JString))
+      elif v is bool:
+        result.add((k, $v, JBool))
+      elif v is array or v is seq:
+        result.add((k, $v, JArray))
+      elif v is RootObj:
+        result.add((k, $v, JObject))
+      else:
+        result.add((k, $v, JNull))
 
 proc fieldsItem*[T](obj: T): seq[FieldItem] =
   for k, v in obj.fieldPairs:
